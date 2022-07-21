aedb905f2b6c802169a841cdaeb23cafc0c3cdca
Gihan Chaturanga Attanayake <76416925+gihanchaturanga@users.noreply.github.com>
2022-07-21 09:04:46 +0530

    profile interface
    profile picture needs to the rouded.

--- app/src/main/java/com/hdp/careup/ProfileActivity.java
+++ app/src/main/java/com/hdp/careup/ProfileActivity.java
@@ -1,8 +1,14 @@
 package com.hdp.careup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +16,33 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.profile_container, new Profile(), "profile loaded");
+        transaction.commit();
+
+
+        ((BottomNavigationView)findViewById(R.id.bottom_navigation)).setOnItemSelectedListener(
+                item -> {
+                    Toast.makeText(getApplicationContext(), "Item "+item.getItemId()+" Has Selected.",
+                            Toast.LENGTH_LONG).show();
+                    System.out.println("Item ID : "+item.getItemId());
+
+                    if(item.getItemId() == 2131362316){
+    //                    transaction.replace(R.id.profile_container, new Profile(), "profile loaded");
+    //                    transaction.commit();
+                    }else if(item.getItemId() == 2131362168){
+    //                    transaction.replace(R.id.profile_container, new Profile(), "profile loaded");
+    //                    transaction.commit();
+                    }
+                    return true;
+                });
+
+        BottomNavigationView navView2 = findViewById(R.id.bottom_navigation);
+        navView2.setOnItemReselectedListener(item -> {
+
+        });
+
     }
 }
